77afcbfcda6ce51b5590f1090659f21ea45992b0
miguel angel <miguelangelvillanueva78@gmail.com>
2023-03-22 18:36:16 +0100

    inserccion de datos

--- main.py
+++ main.py
@@ -20,22 +20,26 @@ con.commit()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS puerto (id integer primary key AUTOINCREMENT, nombre text not null, analisis_id integer, "
                "FOREIGN KEY (analisis_id) REFERENCES analisis(id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS analisis (id integer primary key AUTOINCREMENT, puertos_abiertos int, servicios integer not null, servicios_inseguros integer not null, vulnerabilidades_detectadas integer not null, "
-               "FOREIGN KEY (puertos_abiertos) REFERENCES puerto(id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS responsable (nombre text primary key, tlf integer, rol text not null)")
-cursor.execute("CREATE TABLE IF NOT EXISTS dispositivo (id text primary key, ip text not null, localizacion text, responsable text, analisis integer, FOREIGN KEY(responsable) REFERENCES responsable(nombre), FOREIGN KEY(analisis) REFERENCES analisis(id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS analisis (id integer primary key AUTOINCREMENT, dispositivo text, puertos_abiertos int, servicios integer not null, servicios_inseguros integer not null, vulnerabilidades_detectadas integer not null, "
+               "FOREIGN KEY (dispositivo) REFERENCES dispositivo(id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS responsable (nombre text primary key, tlf integer, rol text )")
+cursor.execute("CREATE TABLE IF NOT EXISTS dispositivo (id text primary key, ip text not null, localizacion text, responsable text, "
+               "FOREIGN KEY(responsable) REFERENCES responsable(nombre))")
 
 con.commit()
-
-
 
 i = 1
 for objeto in datos:
-    print(objeto['responsable'])
     nombre_responsable = objeto['responsable']['nombre']
     tlf_responsable = objeto['responsable']['telefono']
     rol_responsable = objeto['responsable']['rol']
-    cursor.execute("INSERT INTO responsable VALUES (?, ?, ?)", (nombre_responsable, tlf_responsable, rol_responsable))
-    cursor.execute("SELECT * FROM responsable")
-
-con.commit()
+    cursor.execute("INSERT OR IGNORE INTO responsable VALUES (?, ?, ?)", (nombre_responsable, tlf_responsable, rol_responsable))
+    id_dispositivo = objeto['id']
+    ip_dispositivo = objeto['ip']
+    localizacion_dispositivo = objeto['localizacion']
+    cursor.execute("INSERT INTO dispositivo VALUES (?, ?, ?, ?)", (id_dispositivo, ip_dispositivo, localizacion_dispositivo, nombre_responsable))
+    #print(objeto['analisis']['puertos_abiertos'], i)
+    #for puerto in objeto['analisis']['puertos_abiertos']:
+     #   cursor.execute("INSERT INTO puerto VALUES (nombre, analisis_id)", (puerto, i))
+    i = i + 1
+    con.commit()
