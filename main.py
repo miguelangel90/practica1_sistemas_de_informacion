16a8c6b2694f6a6840e2d5924f9dde92f16312da
miguel angel <miguelangelvillanueva78@gmail.com>
2023-03-23 12:01:32 +0100

    tablas de dispositivos añadidas por completo

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ cursor.execute('PRAGMA foreign_keys = ON')
 
 con.commit()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS puerto (id integer primary key AUTOINCREMENT, nombre text not null, analisis_id integer, "
+cursor.execute("CREATE TABLE IF NOT EXISTS puerto (id integer primary key AUTOINCREMENT, nombre text, analisis_id integer, "
                "FOREIGN KEY (analisis_id) REFERENCES analisis(id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS analisis (id integer primary key AUTOINCREMENT, dispositivo text, puertos_abiertos int, servicios integer not null, servicios_inseguros integer not null, vulnerabilidades_detectadas integer not null, "
+cursor.execute("CREATE TABLE IF NOT EXISTS analisis (id integer primary key AUTOINCREMENT, dispositivo text, servicios integer not null, servicios_inseguros integer not null, vulnerabilidades_detectadas integer not null, "
                "FOREIGN KEY (dispositivo) REFERENCES dispositivo(id))")
 cursor.execute("CREATE TABLE IF NOT EXISTS responsable (nombre text primary key, tlf integer, rol text )")
 cursor.execute("CREATE TABLE IF NOT EXISTS dispositivo (id text primary key, ip text not null, localizacion text, responsable text, "
@@ -28,13 +28,16 @@ cursor.execute("CREATE TABLE IF NOT EXISTS dispositivo (id text primary key, ip 
 
 con.commit()
 
-i = 0
+i = 1
+#id_puerto= 0
 ## INSERTAMOS LOS DATOS
 for objeto in datos:
 
     ## TABLA RESPONSABLE
     nombre_responsable = objeto['responsable']['nombre']
     tlf_responsable = objeto['responsable']['telefono']
+    if tlf_responsable == "None":
+        tlf_responsable = None
     rol_responsable = objeto['responsable']['rol']
     cursor.execute("INSERT OR IGNORE INTO responsable VALUES (?, ?, ?)", (nombre_responsable, tlf_responsable, rol_responsable))
 
@@ -42,29 +45,25 @@ for objeto in datos:
     id_dispositivo = objeto['id']
     ip_dispositivo = objeto['ip']
     localizacion_dispositivo = objeto['localizacion']
+    if localizacion_dispositivo == "None":
+        localizacion_dispositivo = None
     cursor.execute("INSERT INTO dispositivo VALUES (?, ?, ?, ?)", (id_dispositivo, ip_dispositivo, localizacion_dispositivo, nombre_responsable))
 
     ## TABLA ANALISIS
     id_analisis= i
-    ## id_dispositivo=
-    puerto_abierto=objeto["analisis"]["puertos_abiertos"]
     servicio_normal=objeto["analisis"]["servicios"]
     servicio_inseguro= objeto["analisis"]["servicios_inseguros"]
     vulnerabilidad_detectada=objeto["analisis"]["vulnerabilidades_detectadas"]
-    cursor.execute("INSERT INTO analisis VALUES (?, ?, ?, ?,?,?)",(id_analisis, ip_dispositivo, puerto_abierto, servicio_normal, servicio_inseguro, vulnerabilidad_detectada))
+    cursor.execute("INSERT INTO analisis(dispositivo, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?)",(id_dispositivo, servicio_normal, servicio_inseguro, vulnerabilidad_detectada))
 
 
     ## TABLA PUERTOS
-    id_puerto= 0
-    ## id_analisis= i
-    for puerto_nuevo in objeto["analisis"]["puertos_abiertos"]:
-        cursor.execute("INSERT INTO puerto VALUES (?, ?, ?)",(id_puerto, puerto_nuevo, id_analisis))
-        id_puerto= id_puerto+1
+    if objeto["analisis"]["puertos_abiertos"] == "None":
+        cursor.execute("INSERT INTO puerto(nombre, analisis_id) VALUES (?, ?)", (None, id_analisis))
+    else:
+        for puerto_nuevo in objeto["analisis"]["puertos_abiertos"]:
+            cursor.execute("INSERT INTO puerto(nombre, analisis_id) VALUES (?, ?)",(puerto_nuevo, id_analisis))
 
-
-    #print(objeto['analisis']['puertos_abiertos'], i)
-    #for puerto in objeto['analisis']['puertos_abiertos']:
-     #   cursor.execute("INSERT INTO puerto VALUES (nombre, analisis_id)", (puerto, i))
 
     i = i + 1
     con.commit()
